0a927b6b3f358599a3a0b029a9389f2947fcd931
“eric” <eric.wijaya@nanovest.io>
2024-05-08 23:36:17 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -142,7 +142,6 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(len(data), 5)
 
-
     def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
@@ -168,3 +167,4 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        
